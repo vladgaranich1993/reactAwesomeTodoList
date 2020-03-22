4d79e9fa209b63174aaf170adfe46953dd4938f3
Влад Гаранич <vladislav.garanich@gmail.com>
2020-03-22 13:40:20 +0100

    set clearing the addForm input on submit and add filter button on SearchPanel

--- src/components/AddTodoForm/AddTodoForm.js
+++ src/components/AddTodoForm/AddTodoForm.js
@@ -16,13 +16,16 @@ export default class AddTodoForm extends Component {
     onSubmitForm = (e) => {
         e.preventDefault();
         this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        })
     }
 
     render() {   
         return(
             <form className="add-todo-form" onSubmit={this.onSubmitForm}>
                 <input className="form-control" onChange={this.onLabelChange} type="text" placeholder="Enter todo title"
-                value={this.state}
+                value={this.state.label}
                 />
                 <button className="btn btn-outline-secondary mt-2">
                     Add Todo
--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -9,6 +9,12 @@ const SearchPanel = () => {
     return(
         <div className="mb-3">
             <input className="form-control" style={searchStyle} placeholder={placeHolderSearch}/>
+            <div className="mt-3">
+                <button type="button" className="mr-2 btn btn-info">All</button>
+                <button className="mr-2 btn btn-primary">Active</button>
+                <button className="btn btn-success">Done</button>
+            </div>
+            
         </div>
     )
 }
